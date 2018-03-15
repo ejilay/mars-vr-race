d81945fddf10bec8ee7f27d8a72a54be69532621
ejilay <ivbaturin@gmail.com>
2018-03-15 21:50:02 +0300

    separate wheel

---
+++ Assets/Scripts/SteeringController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringController : MonoBehaviour {
+
+    public Transform LeftSnapperTransform;
+    public Transform RightSnapperTransform;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
